9b7922639c6c16a45a14d05a4239d50a80684971
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-01-21 20:00:44 +0530

    Userinfo Command!

--- Commands/⚙️-Utility/user-info.js
+++ Commands/⚙️-Utility/user-info.js
@@ -1,14 +1,20 @@
 const Discord = require('discord.js');
 
-module.exports.execute = async (Client, message, args) => {
+module.exports = {
+    name: 'user-info', 
+    description: 'Info about user!',
+    usage: 's!user-info',
+    aliases: ["ui"],
+    execute: async(Client, message, args) => {
+
     let userArray = message.content.split(" ");
     let userArgs = userArray.slice(1);
     let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
 
-    if (member.presence.status === 'dnd') member.presence.status = 'Do Not Disturb';
-    if (member.presence.status === 'online') member.presence.status = 'Online';
-    if (member.presence.status === 'idle') member.presence.status = 'Idle';
-    if (member.presence.status === 'offline') member.presence.status = 'offline';
+    if (member.presence.status === 'dnd') member.presence.status = '<:DO_NOT_DISTURB:801819120378314803> Do Not Disturb';
+    if (member.presence.status === 'online') member.presence.status = ' <:ONLINE:801819271319388180> Online';
+    if (member.presence.status === 'idle') member.presence.status = ' <:IDLE:801819429742182442> Idle';
+    if (member.presence.status === 'offline') member.presence.status = ' <:OFFLINE:801819529930997810> Offline';
 
     let x = Date.now() - member.createdAt;
     let y = Date.now() - message.guild.members.cache.get(member.id).joinedAt;
@@ -29,11 +35,8 @@ module.exports.execute = async (Client, message, args) => {
     .addField("Status", status)
 
     message.channel.send(userEmbed);
-}
 
-module.exports.config = {
-    name: "user-info",
-    description: "Shows the information of a member/user",
-    usage: "s!memberinfo",
-    aliases: ['userinfo', "ui"]
+    }
+
 }
+ 
