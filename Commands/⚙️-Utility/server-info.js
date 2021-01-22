1bb8ca58c1ae18fbed4cdee457498e7ca71d7063
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-01-22 11:09:31 +0530

    Info Commands!

---
+++ Commands/⚙️-Utility/server-info.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: 'server-info',
+    aliases: ['si'],
+    description: "This shows the server info",
+    usage: "s!server-info",
+    run: async (Client, message, args) => {
+        const Discord = require('discord.js');
+
+        const botSize = message.guild.members.cache.filter(m => m.user.bot).size
+
+        const { name, owner, roles, channels, createdAt, premiumSubscriptionCount, memberCount, region } = message.guild
+        message.channel.send(
+            new Discord.MessageEmbed()
+                .setTitle(name)
+                .setDescription(`Some simple server information.`)
+                .setThumbnail(message.guild.iconURL({ dynamic: true }))
+                .addField('Owner', owner, true)
+                .addField('Boosters', premiumSubscriptionCount, true)
+                .addField('Region', region.toUpperCase(), true)
+                .addField('Total Members', memberCount, true)
+                .addField('Total Bots', botSize, true) 
+                .addField('Roles', roles.cache.size, true)
+                .setFooter(`Server Created on`)
+                .setColor('#FF0000')
+                .setTimestamp(createdAt)
+        )
+            .catch((e) => message.channel.send(`error: ${e.message}`))
+    }
+}
--- Commands/⚙️-Utility/user-info.js
+++ Commands/⚙️-Utility/user-info.js
@@ -1,42 +1,60 @@
-const Discord = require('discord.js');
-const moment = require('moment')
+const  {MessageEmbed } = require('discord.js');
+const moment = require('moment');
+const { get } = require('request');
+
+const flags = {
+    DISCORD_EMPLOYEE: 'Discord Employee',
+    DISCORD_PARTNER: 'Discord Partner',
+    BUGHUNTER_LEVEL_1: 'Bug Hunter (Level 1)',
+    BUGHUNTER_LEVEL_2: 'Bug Hunter (Level 2)',
+    HYPESQUAD_EVENTS: 'HypeSquad Events',
+    HOUSE_BRAVERY: 'House of Bravery',
+    HOUSE_BRILLIANCE: 'House of Brilliance',
+    HOUSE_BALANCE: 'House of Balance',
+    EARLY_SUPPORTER: 'Early Supporter',
+    TEAM_USER: 'Team User',
+    SYSTEM: 'System',
+    VERIFIED_BOT: 'Verified Bot',
+    VERIFIED_DEVELOPER: 'Verified Bot Developer'
+};
+
 module.exports = {
-    name: 'user-info', 
-    description: 'Info about user!',
-    usage: 's!user-info',
-    aliases: ["ui"],
-    execute: async(Client, message, args) => {
-
-    let userArray = message.content.split(" ");
-    let userArgs = userArray.slice(1);
-    let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
-
-    if (member.presence.status === 'dnd') member.presence.status = '<:DO_NOT_DISTURB:801819120378314803> Do Not Disturb';
-    if (member.presence.status === 'online') member.presence.status = ' <:ONLINE:801819271319388180> Online';
-    if (member.presence.status === 'idle') member.presence.status = ' <:IDLE:801819429742182442> Idle';
-    if (member.presence.status === 'offline') member.presence.status = ' <:OFFLINE:801819529930997810> Offline';
-
-    let x = Date.now() - member.createdAt;
-    let y = Date.now() - message.guild.members.cache.get(member.id).joinedAt;
-    const joined = Math.floor(y / 86400000);
-
-    const joineddate = moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss");
-    let status = member.presence.status;
-
-    const userEmbed = new Discord.MessageEmbed()
-    .setAuthor(member.user.tag, member.user.displayAvatarURL())
-    .setTimestamp()
-    .setColor('BLUE')
-    .setImage(member.user.displayAvatarURL())
-    .addField("Member ID", member.id)
-    .addField('Roles', `<@&${member._roles.join('> <@&')}>`)
-    .addField("Account Created On:", ` ${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY")}`, true) 
-    .addField('Joined the server At', `${joineddate} \n> ${joined} day(s) Ago`)
-    .addField("Status", status)
-
-    message.channel.send(userEmbed);
-
+    name: 'user-info',
+    description: "Shows a users info",
+    usage: 's!user-info, >user-info <mention>',
+    aliases: ['ui'],
+    run: async (client, message, args) => {
+        const member = message.mentions.members.last() ||  message.member;
+        const roles = member.roles.cache
+            .sort((a, b) => b.position - a.position)
+            .map(role => role.toString())
+            .slice(0, -1);
+        const userFlags = member.user.flags.toArray();
+        if (!member.user.activities) member.user.activities = { name: "none" }
+        const embed = new MessageEmbed()
+            .setThumbnail(member.user.displayAvatarURL({ dynamic: true, size: 512 }))
+            .setColor("#FF0000")
+            .addField('User', [
+                `**❯ Username:** ${member.user.username}`,
+                `**❯ Discriminator:** ${member.user.discriminator}`,
+                `**❯ ID:** ${member.id}`,
+                `**❯ Badges:** ${userFlags.length ? userFlags.map(flag => flags[flag]).join(', ') : 'None'}`,
+                `**❯ Avatar:** [Link to avatar](${member.user.displayAvatarURL({ dynamic: true })})`,
+                `**❯ Time Created:** ${moment(member.user.createdTimestamp).format('LT')} ${moment(member.user.createdTimestamp).format('LL')} ${moment(member.user.createdTimestamp).fromNow()}`,
+                `**❯ Status:** ${member.user.presence.status}`,
+                `**❯ Activity:** *${member.user.presence.activities[0] || '`No Activity`'}*`, 
+                `**❯ Nickname:** ${member.nickname || 'None'}`,
+                `\u200b`
+            ])
+            .addField('Member', [
+                `**❯ Highest Role:** ${member.roles.highest.id === message.guild.id ? 'None' : member.roles.highest.name}`,
+                `**❯ Server Join Date:** ${moment(member.joinedAt).format('LL LTS')}`,
+                `**❯ Hoist Role:** ${member.roles.hoist ? member.roles.hoist.name : 'None'}`,
+                `**❯ Roles [${roles.length}]:** ${roles.length  ? roles.join(', ') : 'None'}`,
+                `\u200b`
+            ])
+            .setFooter(`Requested by ${message.author.username}` , `${message.author.displayAvatarURL( {dynamic : true} )}`);
+        return message.channel.send(embed);
     }
-
 }
  
