c40f480dedf436c569021bc6e8bf428c282a0839
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-01-26 19:22:29 +0530

    Fixed 8Ball and added Flip

---
+++ Commands/🎮-Fun/flip.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+module.exports = {
+    name: 'flip',
+    description: 'Flip a coin!',
+    usage: 's!flip',
+    examples: 's!flip',
+    execute: async(Client, message, args) => {
+        let replies = ["Heads", "Tails", ];
+
+        let result = Math.floor((Math.random() * replies.length));
+
+        message.channel.send('Fliping a Coin').then(msg => msg.edit(`> :coin: The Result was **${result}**`))
+    }
+}
