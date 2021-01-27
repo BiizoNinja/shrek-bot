c8525d28e4b441eb08dcdeaf3ddb6ede9dc2e05f
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-01-27 22:17:53 +0530

    Added Hack, Fixed Flip

---
+++ Commands/🎮-Fun/hack.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js')
+module.exports = {
+    name: 'hack',
+    description: 'hack others',
+    usage: 's!hack <user/userid>',
+    examples: 's!hack @BiizoNinja',
+    execute: async(Client, message, args) => {
+        const user = message.mentions.users.first() 
+
+        if(!user) return message.channel.send('provide a user to hack ;/') 
+        if(user) {
+         const emails = ["bestguy@gmail.com", "simp1234@hotmail.com", "iliketoeatwood34@gmail.com","fatdiscorddev@outlook.com", `${user.username}_is_single@gmail.com`, "someone@poo.com" ]
+         const emailRandomiser = Math.floor((Math.random()* emails.length));
+
+         const passwords = [`${user.username}iscool123`, "hahaImGay@#$", "P@SSW0RD"]
+         const passowrdRandomiser = Math.floor((Math.random()* passwords.length));
+
+         const msg = await message.channel.send(`Hacking ${user.tag}`);
+         setTimeout(() => {
+             let embed = new Discord.MessageEmbed()
+             .setTitle('Success Fully Hacked User!')
+             .addFields (
+                 {name: 'Email' , value: `**${emails[emailRandomiser]}**` },
+                 {name: 'Password', value:`**${passwords[passowrdRandomiser]}**`}
+             )
+             .setColor(' #00ffff')
+             .setFooter(` Totally Real hack ;)`)
+             msg.edit(embed)
+         }, 2000);
+
+        }
+
+
+        
+    }
+}
