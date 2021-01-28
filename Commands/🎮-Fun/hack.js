4774967f5b5097e8a0e601b1a059618eb259c378
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-01-28 09:53:54 +0530

    NSFW FIXED in Hack

--- Commands/🎮-Fun/hack.js
+++ Commands/🎮-Fun/hack.js
@@ -9,10 +9,10 @@ module.exports = {
 
         if(!user) return message.channel.send('provide a user to hack ;/') 
         if(user) {
-         const emails = ["bestguy@gmail.com", "simp1234@hotmail.com", "iliketoeatwood34@gmail.com","fatdiscorddev@outlook.com", `${user.username}_is_single@gmail.com`, "someone@poo.com" ]
+         const emails = ["bestguy@gmail.com", "simp1234@hotmail.com", "iliketoeatwood34@gmail.com","fatdiscorddev@outlook.com", `${user.username}_is_single@gmail.com`, "someone@poo.com", "Icandostuff@Moonmail.com", `potatoesarepog@gamerlang.co` ]
          const emailRandomiser = Math.floor((Math.random()* emails.length));
 
-         const passwords = [`${user.username}iscool123`, "hahaImGay@#$", "P@SSW0RD"]
+         const passwords = [`${user.username}iscool123`, "1234567890", "P@SSW0RD"]
          const passowrdRandomiser = Math.floor((Math.random()* passwords.length));
 
          const msg = await message.channel.send(`Hacking ${user.tag}`);
