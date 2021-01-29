b1fea2cb6d815b10a7ef8a12e8168ee336dc7869
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-01-29 20:10:13 +0530

    Added ship

---
+++ Commands/🎮-Fun/ship.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+module.exports = {
+  name: "ship",
+  description: "ship someone!",
+  usage: "s!ship",
+  run: async (bot, message, args) => {
+    let ship = Math.floor(Math.random() * 100) + 1;
+
+    let user = message.mentions.users.first();
+    let robber = message.author;
+
+    if (!user) {
+      return message.channel.send(
+        "Make sure you pick a person who you want to ship!"
+      );
+    }
+
+    let embed = new Discord.MessageEmbed()
+      .setTimestamp(Date.now())
+      .setTitle("Hmmmm who are we Shipping today?")
+      .setDescription(
+        `**${robber.username}** & **${user.username}** your match is... ${ship}%`
+      )
+      .setColor(`RANDOM`);
+    message.channel.send(embed).then((m) => {
+      m.react("❤");
+      m.react("💙");
+    });
+  },
+};
