8db73b1eee5e7f3d3893bcabf0d36f5b5c9fef61
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-01-30 11:53:30 +0530

    Updates

---
+++ Commands/DEVELOPER ONLY/restart.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js")
+const config = require('../../config.json')
+const token = config.token
+
+module.exports = {
+ name: 'restart',
+ description: '**ONLY DEVELOPER**',
+ execute: async(Client, message, args) => {
+
+    if(message.author.id !== "546631496673394688") return
+
+    Client.destroy()
+    await Client.login(token)
+
+    message.channel.send('Restarted!')
+    
+
+
+    
+ }
+}
--- Commands/⚙️-Utility/invite.js
+++ Commands/⚙️-Utility/invite.js
@@ -12,7 +12,8 @@ module.exports = {
        .setDescription(`ShrekBot is a multi-purpose discord bot. It has memes, levelling economy with customizable rewards! Add it you your discord server today!`)
        .addFields(
            {name:'<a:gold_arrow:799265384188477521> Link to invite', value:`[Click here!](https://bit.ly/ShrekInvite)`},
-           {name:'<a:loadingred:799265479112916993> Support Server', value:`[Click here!](https://discord.gg/CgzBqZjz2v)`}
+           {name:'<a:loadingred:799265479112916993> Support Server', value:`[Click here!](https://discord.gg/CgzBqZjz2v)`},
+           {name: '<a:topgg:804957699506831391> Vote here', value: `[Vote here!](https://top.gg/bot/789129116015525918`}
        )
        .setColor('#00f6e3')
        .setFooter(`Requested By ${message.author.tag}  •  Made By BiizoNinja#3337`, message.author.displayAvatarURL({ dynamic: true }))
