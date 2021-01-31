7c2c21a37d2fe9fe2730533ae5f8c2052e2fb250
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-01-31 16:34:29 +0530

    Update help.js

--- Commands/⚙️-Utility/help.js
+++ Commands/⚙️-Utility/help.js
@@ -21,10 +21,16 @@ module.exports = {
                     value: cmds.length === 0 ? "In Progress..." : cmds.join(',')
                 }
                 categories.push(data)
+
             })
+            const data = await prefix.findOne({
+                GuildId: message.guild.id
+            });
+            const prefix = data.Prefix;
+            
             let embed = new Discord.MessageEmbed()
             .setTitle('Help Command!')
-            .setDescription(` Do help <command> for more info!`)
+            .setDescription(` Do ${prefix}help <command> for more info!`)
             .addFields( categories,
                 { name: "\u200c", value: ("<a:Arrow_pointing_right:769978144760791082> [Invite](https://dsc.gg/shrekbot) `|` <a:wumpus_coding:801002702552170506> [Support Server](https://discord.gg/CgzBqZjz2v) `|` <a:topgg:804957699506831391> [Vote Here!](https://top.gg/bot/789129116015525918)" ), inline: true },
             )
