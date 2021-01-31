b37108f2fd7ac70c64cd20e1243af2e714728eee
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-01-31 20:13:54 +0530

    Update trash.js

--- Commands/🎮-Fun/trash.js
+++ Commands/🎮-Fun/trash.js
@@ -7,6 +7,9 @@ module.exports = {
     usage: 's!baguette <text>',
     examples: 's!baguette Hello',
     execute: async(Client, message, args) => {
+        const user = message.mentions.users.first()
+
+        if(!user) {
         const avatarURL = message.author.displayAvatarURL({dynamic: true})
         fetch(`https://nekobot.xyz/api/imagegen?type=trash&url=${avatarURL}`)
             .then((res) => res.json())
@@ -17,6 +20,21 @@ module.exports = {
                     .setColor("GOLD")
                 message.channel.send(embed)
             })
+
+        }
+        if(user) {
+            const userURL = message.user.displayAvatarURL({dynamic: true})
+            fetch(`https://nekobot.xyz/api/imagegen?type=trash&url=${userURL}`)
+            .then((res) => res.json())
+            .then((body) => {
+
+                let embed = new MessageEmbed()
+                    .setImage(body.message)
+                    .setColor("GOLD")
+                message.channel.send(embed)
+            })
+        }
+
 
                 
    }
