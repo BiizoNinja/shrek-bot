c5b59c1875a386334588081d8047bb88177d38ed
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-01 09:37:44 +0530

    Update ping.js

--- Commands/⚙️-Utility/ping.js
+++ Commands/⚙️-Utility/ping.js
@@ -3,11 +3,11 @@ module.exports = {
    description: "Gets the bot ping",
    usage: 's!ping',
    aliases: ['beep'],
-   run: async (client, message, args) => {
+   execute: async (Client, message, args) => {
        const Discord = require('discord.js');
 
 
-       message.channel.send('<a:loading:798947890257068093>').then(pingMessage => {
+       message.channel.send('<a:red_loading:803500709713543178>').then(pingMessage => {
 
            const start = message.createdTimestamp;
            const end = pingMessage.createdTimestamp;
@@ -16,8 +16,8 @@ module.exports = {
            let pingembed = new Discord.MessageEmbed()
                .setColor('#FF0000')
                .setTitle(`🏓**Pong!**`)
-               .setDescription(`Bot Latency: \`${subtraction}ms.\`\n API Latency: \`${Math.round(message.client.ws.ping)}ms\``)
-               .setFooter('Is it okay? I can\'t look!')
+               .setDescription(`Bot Latency: \`${subtraction}ms.\`\n API Latency: \`${Math.round(message.Client.ws.ping)}ms\``)
+               .setFooter(`Requested By: ${message.author.tag}`, message.author.displayAvatarURL({dyanamic: true}));
 
            pingMessage.delete()
            message.channel.send(pingembed)
