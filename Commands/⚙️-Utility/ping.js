1c0a57eccdb5c352c16fbf43672f45073c528187
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-01 09:34:53 +0530

    Update ping.js

--- Commands/⚙️-Utility/ping.js
+++ Commands/⚙️-Utility/ping.js
@@ -1,31 +1,29 @@
-const Discord = require('discord.js')
 module.exports = {
    name: 'ping',
-   description: 'Shows the bot\'s ping',
-   aliases: ["beep"],
+   description: "Gets the bot ping",
    usage: 's!ping',
-   execute:async(Client, message, args) => {
+   aliases: ['beep'],
+   run: async (client, message, args) => {
+       const Discord = require('discord.js');
 
-    await message.channel.send('PINGING!').then(pingMessage => {
-            
-        const start = message.createdTimestamp;
-        const end = pingMessage.createdTimestamp;
-        const subtraction = end - start;
 
-        let PingEmbed = new Discord.MessageEmbed()
-        
-           .setTitle('My ping! <:ShrekHappy:789749401169559563>')
-           .setDescription(` My ping is ${subtraction} ms`)
-           .setFooter(`Requested By ${message.author.tag}`, message.author.displayAvatarURL({ dynamic: true }))
-           .setColor(' #17ab00 ')
-           
-    
-        
-        pingMessage.edit(PingEmbed)
-        })
+       message.channel.send('<a:loading:798947890257068093>').then(pingMessage => {
 
+           const start = message.createdTimestamp;
+           const end = pingMessage.createdTimestamp;
+           const subtraction = end - start;
+
+           let pingembed = new Discord.MessageEmbed()
+               .setColor('#FF0000')
+               .setTitle(`🏓**Pong!**`)
+               .setDescription(`Bot Latency: \`${subtraction}ms.\`\n API Latency: \`${Math.round(message.client.ws.ping)}ms\``)
+               .setFooter('Is it okay? I can\'t look!')
+
+           pingMessage.delete()
+           message.channel.send(pingembed)
+       })
+
+   }
 }
-}
-   
 
 
