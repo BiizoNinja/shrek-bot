afc05ae41ae99004f57431b81565c3237260417d
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-02 20:37:38 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const { mongoPath } = require('./config.json')
 
 module.exports = async () => {
-  await mongoose.connect(mongoPath, {
+  await mongoose.connect('mongodb+srv://BiizoNinja:Shashank2007!@cluster0.th9eb.mongodb.net/Data', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
