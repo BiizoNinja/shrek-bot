4bb2ddd3816702c415205216ea6135f422c05b6c
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-02 12:16:40 +0530

    Update emoji-info.js

--- Commands/⚙️-Utility/emoji-info.js
+++ Commands/⚙️-Utility/emoji-info.js
@@ -10,12 +10,15 @@ module.exports = {
      if(!emoji) return message.channel.send('<a:XMARK:801450921112371232> You have not supplied an emoji') 
      if(emoji.animated === true) return 'True'
      if(emoji.animated === false) return 'False'
+     const emojiCreatedTimestamp = new Date(emoji.createdTimestamp).toUTCString()
      if(emoji) {
          let embed = new Discord.MessageEmbed()
          .setTitle('Emoji Info!')
          .setDescription('Here is some info about the emoji!') 
          .addFields(
-             {name: 'Animated?' , value: `${emoji.animated}`}
+             {name: 'Animated?' , value: `${emoji.animated}`},
+             {name: 'Name + ID' , value: `${emoji.name} ${emoji.id}` , inline: true},
+             {name: 'Created At', value: `${emojiCreatedTimestamp}`}
 
          )
          message.channel.send(embed)
