286013a9fb89b29ac9282e0a9e021d1a60de9a4b
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-03 10:19:03 +0530

    Removed setprefix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { Intents } = require("discord.js")
 const fs = require('fs')
 const mongo = require('./mongo')
 const mongoose = require('mongoose');
-mongoose.connect(mongo)
 
 const Client = new Discord.Client({
     fetchAllMembers: true,
@@ -50,16 +49,6 @@ fs.readdirSync('./Commands/.').forEach(dir => {
 console.log(table.toString())
 
 Client.on('message', async message => {
-    if(message.content === '<@!789129116015525918> help ') {
-        message.reply('Do `s!help` for a full list of commands!')
-    }
-    if(message.content === '<@!789129116015525918> ') {
-        message.channel.send(' My prefix is `s!`')
-        
-    }
-    const data = await prefix.findOne({
-        GuildID: message.guild.id
-    });
 
     if(message.author.bot) return;
 
@@ -67,48 +56,21 @@ Client.on('message', async message => {
     let cmd = args.shift().toLowerCase()
     let command = Client.commands.get(cmd)
 
-    if(data) {
-        const prefix = data.Prefix;
-
-        if (!message.content.startsWith(prefix)) return;
-        if(!message.guild) return;
-
-        let args = message.content.slice(prefix.length).trim().split(' ')
-        let cmd = args.shift().toLowerCase()
-        let command = Client.commands.get(cmd)
-
-        const ping = ["<@!789129116015525918>"];
-        if (ping.some(word => message.content.includes(word))) {
-            message.reply(`My prefix for this server is \`${prefix}\`. Do \`${prefix}help\` for a list of commands!`);
-        }
-
-        const commandfile = Client.commands.get(cmd.slice(prefix.length)) || Client.commands.get(Client.aliases.get(cmd.slice(prefix.length)));
-        if (!command) command = Client.commands.get(Client.aliases.get(cmd));
-        command.execute(Client, message, args);
-
-    } else if (!data) {
-        //set the default prefix here
-        const prefix = "s!";
+    const prefix = "s!";
         
-        if (!message.content.startsWith(prefix)) return;
-        if(!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if(!message.guild) return;
 
 
-        let args = message.content.slice(prefix.length).trim().split(' ')
-        let cmd = args.shift().toLowerCase()
-        let command = Client.commands.get(cmd)
+    let args = message.content.slice(prefix.length).trim().split(' ')
+    let cmd = args.shift().toLowerCase()
+    let command = Client.commands.get(cmd)
 
-        const ping = ["<@!789129116015525918>"];
-        if (ping.some(word => message.content.includes(word))) {
-            message.reply(`My prefix for this server is \`s!\`. Do \`s!help\` for a list of commands!`);
-        }
-
-        const commandfile = Client.commands.get(cmd.slice(prefix.length)) || Client.commands.get(Client.aliases.get(cmd.slice(prefix.length)));
-        if (!command) command = Client.commands.get(Client.aliases.get(cmd));
-        command.execute(Client, message, args);
-    }
+    const commandfile = Client.commands.get(cmd.slice(prefix.length)) || Client.commands.get(Client.aliases.get(cmd.slice(prefix.length)));
+    if (!command) command = Client.commands.get(Client.aliases.get(cmd));
+    command.execute(Client, message, args);
+    
 })
-
 
 Client.login(process.env.token)
 
