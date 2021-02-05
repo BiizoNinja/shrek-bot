f8f6e5919b3aefae343951810d17f249d9f2a405
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-05 17:17:27 +0530

    Update bet.js

--- Commands/🪙-Economy/bet.js
+++ Commands/🪙-Economy/bet.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'bet',
     description: 'bet your coins ',
     usage: 's!bet <amount-of-coins>',
-    timeout: 300000,
+    timeout: 60000,
     aliases: [],
     execute: async(Client, message, args) => {
         const userID = message.author.id
@@ -17,9 +17,24 @@ module.exports = {
         if(isNaN(amount)) return message.channel.send(`bro you need to bet a number in your wallet.`)
 
         const amountToBet = parseInt(amount)
-        const bal = economy.getCoins(userID, guildID)
+        const bal = economy.getCoins(userID)
 
-        console.log(`${message.author.tag} USED THE COMMAND in ${message.guild.name}`)
-        message.reply('IN PROGRESS :eyes:')
+        if(bal(message.author.id) < amountToBet) return message.channel.send(`you don't that kinda money in yer wallet. How can you bet something you don't have? :thinking:`)
+
+        function random() {
+            const num = Math.floor(Math.random()* 2)
+            return num === 1;
+
+        }
+        if(random() === true) {
+            const winAmout = amountToBet * 2
+            message.channel.send(`gg, you won **${winAmout}** <:Shrek_Coin:807140320507854878>. atleast your luck is better than my future :clap: `)
+            await economy.addCoins(userID, winAmout)
+
+        } else {
+            message.channel.send(` haha i have a lucky charm! You just lost **${amountToBet}** coins :rofl: try again next time kid`)
+            await economy.removeCoins(userID, amountToBet)
+
+        }
     }
 }
