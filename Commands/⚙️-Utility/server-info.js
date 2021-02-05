5a59f74c51a190f4665cc0eb20ef340022b72c69
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-05 18:14:37 +0530

    Update server-info.js

--- Commands/⚙️-Utility/server-info.js
+++ Commands/⚙️-Utility/server-info.js
@@ -6,24 +6,24 @@ module.exports = {
     execute: async (Client, message, args) => {
         const Discord = require('discord.js');
 
-        const botSize = message.guild.members.cache.filter(m => m.user.bot).size
-
-        const { name, owner, roles, channels, createdAt, premiumSubscriptionCount, memberCount, region } = message.guild
-        message.channel.send(
-            new Discord.MessageEmbed()
-                .setTitle(name)
-                .setDescription(`Some simple server information.`)
-                .setThumbnail(message.guild.iconURL({ dynamic: true }))
-                .addField('Owner', owner, true)
-                .addField('Boosters', premiumSubscriptionCount, true)
-                .addField('Region', region.toUpperCase(), true)
-                .addField('Total Members', memberCount, true)
-                .addField('Total Bots', botSize, true) 
-                .addField('Roles', roles.cache.size, true)
-                .setFooter(`Server Created on`)
-                .setColor('RANDOM')
-                .setTimestamp(createdAt)
-        )
-            .catch((e) => message.channel.send(`error: ${e.message}`))
+        var EMBED = new Discord.MessageEmbed()
+        .setTitle("server info")
+        .addField("server name 🎗️", `${message.guild.name}`)
+        .addField("server id 🆔", `${message.guild.id}`)
+        .addField("server owner 👑", `${message.guild.owner}`)
+        .addField("members 👥", `${message.guild.memberCount}`)
+        .addField("Server roles 🔐", `${message.guild.roles.cache.size}`)
+    .addField(" channels 💬", `  ${message.guild.channels.cache.filter(r => r.type === "text").size} Text
+          ${message.guild.channels.cache.filter(r => r.type === "voice").size} Voice`)
+        .addField("server region 🌍", `${message.guild.region}`)  
+        .addField("Verification Level 📑", `${message.guild.verificationLevel}`)
+    .addField("created in 📆 ", `${message.guild.createdAt.toLocaleString()}`)
+    .addField("Boosts ✨", `${message.guild.premiumSubscriptionCount}`)
+    .setColor("RANDOM")
+    .setFooter(`Requsted by ${message.author.username}`, message.member.displayAvatarURL( {dynamic: true}))
+    .setThumbnail(message.guild.iconURL({ dynamic: true }))
+        message.channel.send(EMBED)
+      }
+    
     }
-}
+
