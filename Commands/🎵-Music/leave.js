f7f08430874b0d640785105b8604a50761c2c42d
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-07 12:22:39 +0530

    Added basic music

---
+++ Commands/🎵-Music/leave.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'leave',
+    description: 'stop the bot and leave the channel',
+    usage: 's!leave',
+    examples: 's!leave',
+    aliases: ["disconnect"],
+    execute: async(message, args) => {
+        const voiceChannel = message.member.voice.channel;
+ 
+        if(!voiceChannel) return message.channel.send("You need to be in a voice channel to stop the music!");
+        await voiceChannel.leave();
+        await message.channel.send('Leaving channel :smiling_face_with_tear:')
+ 
+    }
+}
---
+++ Commands/🎵-Music/play.js
@@ -0,0 +1,72 @@
+const ytdl = require('ytdl-core')
+const ytSearch = require('yt-search')
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'play',
+    description: "play a song!",
+    timemout: 10000,
+    usage: 's!play <song name | url>',
+    examples: 's!play cool song',
+    aliases: ["p"],
+    execute: async(Client, message, args) => {
+        const voiceChannel = message.member.voice.channel;
+ 
+        if (!voiceChannel) return message.channel.send('You need to be in a channel to execute this command!');
+        const permissions = voiceChannel.permissionsFor(message.Client.user);
+        if (!permissions.has('CONNECT')) return message.channel.send('You dont have the correct permissins');
+        if (!permissions.has('SPEAK')) return message.channel.send('You dont have the correct permissins');
+        if (!args.length) return message.channel.send('You need to send the second argument!');
+ 
+        const validURL = (str) =>{
+            var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+            if(!regex.test(str)){
+                return false;
+            } else {
+                return true;
+            }
+        }
+ 
+        if(validURL(args[0])){
+ 
+            const  connection = await voiceChannel.join();
+            const stream  = ytdl(args[0], {filter: 'audioonly'});
+ 
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () =>{
+                voiceChannel.leave();
+                message.channel.send('leaving channel');
+            });
+ 
+            await message.reply(`:thumbsup: Now Playing ***Your Link!***`)
+ 
+            return
+        }
+ 
+        
+        const  connection = await voiceChannel.join();
+ 
+        const videoFinder = async (query) => {
+            const videoResult = await ytSearch(query);
+ 
+            return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+ 
+        }
+ 
+        const video = await videoFinder(args.join(' '));
+ 
+        if(video){
+            const stream  = ytdl(video.url, {filter: 'audioonly'});
+            connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () =>{
+                voiceChannel.leave();
+            });
+ 
+            await message.reply(`:thumbsup: Now Playing ***${video.title}***`)
+        } else {
+            message.channel.send('No video results found');
+        }
+    }
+}
+ 
+    
