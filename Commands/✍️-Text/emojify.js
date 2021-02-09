074a2d0d0d3a836bc59b9b457086eff939467d62
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-09 10:32:28 +0530

    Added Emojify Command!

---
+++ Commands/✍️-Text/emojify.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+const emoji = require('discord-emoji-convert')
+
+module.exports = {
+    name: 'emojify',
+    description: 'Makes your text into emojies!',
+    aliases: ["emoji"],
+    usage: 's!emojify <message>',
+    examples: 's!emojify hello',
+    execute: async(Client, message, args) => {
+        const fullMessage = args.join(' ')
+
+        if(!fullMessage) return message.channel.send(`You need to specify something to emojify.`)
+
+        const result = emoji.convert(fullMessage)
+        message.channel.send(result)
+    }
+ }
