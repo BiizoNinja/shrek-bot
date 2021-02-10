d5649e601064f95be6ed893ad164376d20ead430
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-10 18:25:25 +0530

    Update balance.js

--- Commands/🪙-Economy/balance.js
+++ Commands/🪙-Economy/balance.js
@@ -13,7 +13,7 @@ module.exports = {
 
         const userID = target.id;
 
-        const coins = await economy.getCoins(userID, coins)
+        const coins = await economy.getCoins(userID)
 
         let embed = new Discord.MessageEmbed()
         .setTitle(`${target.tag}'s Balance!`)
