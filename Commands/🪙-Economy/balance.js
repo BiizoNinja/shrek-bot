068fd6d778c74b5e327003f3a12e291ef8263350
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-21 18:31:03 +0530

    Update balance.js

--- Commands/🪙-Economy/balance.js
+++ Commands/🪙-Economy/balance.js
@@ -1,13 +1,25 @@
 const Discord = require('discord.js')
+const profileSchema = require('../../models/profileSchema')
 
 module.exports = {
     name: 'balance',
     description: 'Check your balance!',
-    usage: 's!bal [@user]',
-    examples: 's!bal || s!bal @BiizoNinja',
+    usage: 's!bal ',
+    examples: 's!bal',
     aliases: ["bal", "coins"],
-    execute: async(Client, message, args, profileData) => {
+    execute: async(Client, message, args) => {
 
+        let profileData;
+        try{
+            profileData = profileSchema.findOne({ userId: message.author.id})
+            if(!profileData) {
+                userId: message.author.id
+                coins: 0
+                bank: 0 
+            }
+        } catch(err) {
+        console.log(err)
+        }
     const embed = new Discord.MessageEmbed()
     .setTitle(`${message.author.tag}'s Balance!`, message.author.displayAvatarURL({dynamic: true}))
     .setDescription(`**Wallet:** ${profileData.coins}\n **Bank:** ${profileData.bank}`)
