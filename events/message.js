6ece11941f6d3a684f202f598bafbe1d2c420f36
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-23 17:31:49 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -10,7 +10,6 @@ client.on('message', async message =>{
     if(!message.guild) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
-    if(cmd.length == 0 ) return;
     let command = client.commands.get(cmd)
     if(!command) command = client.commands.get(client.aliases.get(cmd));
     if (command) {
