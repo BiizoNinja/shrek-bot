4e586d46ec647567a758b42065be26c4d945e6e9
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-25 11:52:13 +0530

    Create joinedFirst.js

---
+++ Commands/⚙️-Utility/joinedFirst.js
@@ -0,0 +1,52 @@
+const { ReactionPages }  = require('reconlx');
+const { Client, Message, MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'joinedFirst',
+    description: 'findout who join first in the server!',
+    usage: 'joinedFirst',
+    aliases: ['joinedfirst'],
+    run: async(client, message, args) => {
+        const members = message.guild.members.cache
+            .filter((m) => !m.user.bot)
+            .sort((a, b) => a.joinedTimestamp - b.joinedTimestamp);
+
+        const arrayOfMembers = members.array();
+        const ids = [];
+        arrayOfMembers.forEach((mem) => {
+            ids.push(mem.user.id);
+        })
+
+        let index = 1;
+        if(ids.length > 10) {
+            const chunks = convertChunk(ids, 10);
+            const arry = [];
+            for (chunk of chunks) {
+                const description = chunk.map((v) => `#${index++} **${message.guild.members.cache.get(v).user.tag}**`);
+                arry.push(
+                    new MessageEmbed()
+                        .setTitle('Join Leaderboard in ' + message.guild.name)
+                        .setDescription(description)
+                        .setColor('RANDOM')
+                )
+            }
+            ReactionPages(message, arry, true)
+        } else {
+            const description = ids.map((v) => `#${index++} **${message.guild.members.cache.get(v).user.tag}**`);
+            message.channel.send(
+                new MessageEmbed()
+                 .setTitle('Join Leaderboard in ' + message.guild.name)
+                 .setDescription(description)
+                 .setColor('RANDOM')
+            )
+        }
+    }
+}
+
+function convertChunk(arr, size) {
+    const array = [];
+    for (let i = 0; i < arr.length; i+= size) {
+        array.push(arr.slice(i, i+size))
+    }
+    return array;
+}
