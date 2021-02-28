b1ee3f9ff066a10b3c1a7da1e3a9aa46eb882b6b
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-28 12:46:13 +0530

    Create slap.js

---
+++ Commands/🎮-Fun/slap.js
@@ -0,0 +1,29 @@
+// https://vacefron.nl/api//batmanslap?[&batman=USER_AVATAR][&robin=USER_AVATAR]
+
+const Discord = require('discord.js');
+const fetch  = require('node-fetch')
+
+module.exports = {
+name: 'slap',
+description: 'slaps users!',
+usage: 's!slap <@user>',
+cooldown: 0 ,
+
+run: async (client, message, args) => {
+    const target  = message.mentions.users.first()
+    if(!target) return message.channel.send('Please specify a user to slap...')
+
+    const authorAvatarUrl = message.member.displayAvatarURL()
+    const targetURL = target.displayAvatarURL()
+
+    fetch(`https://vacefron.nl/api//batmanslap?[&batman=${authorAvatarUrl}][&robin=${targetURL}]`)
+    .then((res) => res.json())
+    .then((body) => {
+        let embed = new MessageEmbed()
+        .setImage(body.message)
+        .setColor("RANDOM")
+    message.channel.send(embed)
+    })
+
+}
+}
