c711bdb2b28941d79643d35ea4fbf90aa0b3d599
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-02-28 21:09:13 +0530

    Update inventoryDataSchema.js

--- models/inventoryDataSchema.js
+++ models/inventoryDataSchema.js
@@ -1,12 +1,10 @@
-const mongoose = require('mongoose')
+const {model, Schema} = require('mongoose')
 
 
-const invData = new mongoose.Schema({
-    userId: ({
-        type: String,
-        required: true
-    }),
+ module.exports = model("inventory", 
+ new Schema ({
+    userId: String,
     inventory: Object
 })
-
-module.exports = mongoose.model('inventoryData', invData )
+);
+   
