2229c4e9cd5a8f6d9fde3a16fc2732cbc19d7e15
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-03-06 13:43:34 +0530

    Update jail.js

--- Commands/🖼️-Image/jail.js
+++ Commands/🖼️-Image/jail.js
@@ -12,7 +12,7 @@ run: async (client, message, args) => {
  const user = message.author;
  const avatar = user.displayAvatarURL({dynamic: false, format: 'png'})
 
- const image = await Canvas.jail(avatar, greyscale = false)
+ const image = await Canvas.jail(avatar)
  const imageSend = new Discord.MessageAttachment(image, "image.png")
  message.channel.send(imageSend)
 
