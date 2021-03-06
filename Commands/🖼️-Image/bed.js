a7e48d3540145682475a01549adddf107af351d9
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-03-06 12:49:18 +0530

    Commands

---
+++ Commands/🖼️-Image/bed.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const {Canvas} = require('canvacord')
+
+module.exports = {
+name: 'bed',
+description: 'bed',
+usage: 's!bed <@user>',
+cooldown: 0 ,
+aliases: [''],
+run: async (client, message, args) => {
+ const user = message.author;
+ const avatar = user.displayAvatarURL({dynamic: false, format: 'png'})
+
+ const target = message.mentions.users.first()
+ const avatar2 = target.displayAvatarURL({dynamic: false, format: 'png'})
+
+ const image = await Canvas.bed(avatar, avatar2)
+ const imageSend = new Discord.MessageAttachment(image, "bed.png")
+ message.channel.send(imageSend)
+
+}
+}
