f8231523c95b6a4465423e41e7df628c898b318e
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-03-06 13:23:38 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ client.on('ready', () => {
     console.log(`${client.user.tag} is now online!`);
 
     setInterval(() => {
-        client.user.setActivity(`Prefix is CHANGED! IT IS NOW  \`+\`, {type: 'PLAYING'})
+        client.user.setActivity(`Prefix is CHANGED! IT IS NOW  \`+\``, {type: 'PLAYING'})
     }, 40000)
 })
