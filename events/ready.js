809ec5986fe01d795dc70730bb9e45af8003a2e6
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-03-09 10:27:28 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,20 +3,9 @@ const client = require('../index');
 client.on('ready', () => {
     console.log(`${client.user.tag} is now online!`);
 
-    const activity = [
-       `${client.guilds.cache.size} servers!`,
-       `pog gamers`
-        ]   
-
 
             setInterval(()=>{
-                const index = Math.floor(i);
-                client.user.setActivity(activity[index],{type: "WATCHING"})
-                console.log(i);
-                i = i +1;
-                console.log(i);
-                if(i === activity.length) i = i -activity.length;
-        
+                client.user.setActivity(`${client.guilds.cache.size} servers! `,{type: "WATCHING"})
             },30000)
            
         });
