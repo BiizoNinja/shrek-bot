d25571b6d5d8f7c365ac4a560b6b68b67328a66a
BiizoNinja <74667437+BiizoNinja@users.noreply.github.com>
2021-07-31 10:55:40 +0530

    fixed bold.js

--- Commands/Text/bold.js
+++ Commands/Text/bold.js
@@ -11,12 +11,6 @@ module.exports = {
     if (!fullMessage)
       return message.reply("You need to specify something to bold.");
 
-    name: 'bold',
-    description: 'Makes your messages in to **bold**',
-    aliases: ["bold"],
-    usage: 'boldify <message>',
-    cooldown: 0,
-    run: async (client, message, args) => {
         const fullMessage = args.slice(0).join(" ")
         if(fullMessage.includes('@everyone') || fullMessage.includes('@here')) return message.channel.send('<:wrong:856162786319925270> You aren\'t allowed to use this to ping everyone!')
         if(fullMessage.includes('<@&')) return message.channel.send('<:wrong:856162786319925270> You aren\'t allowed to use this to ping everyone!')
@@ -27,4 +21,4 @@ module.exports = {
       `**${fullMessage}**\n\nRequested by: ${message.author.tag}`
     );
   },
-};
+}
