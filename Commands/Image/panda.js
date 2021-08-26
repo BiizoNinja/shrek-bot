25b47fe7989002982b4c4c48ad566cf3425f7be3
Arche <66189242+arche-dev@users.noreply.github.com>
2021-08-26 23:08:40 +0000

    Ident panda.js

--- Commands/Image/panda.js
+++ Commands/Image/panda.js
@@ -2,22 +2,22 @@ const Discord = require('discord.js');
 const axios = require('axios')
 
 module.exports = {
-name: 'panda',
-description: 'Sends a panda image and a fact!',
-usage: 'panda',
-run: async (client, message, args) => {
+  name: 'panda',
+  description: 'Sends a panda image and a fact!',
+  usage: 'panda',
+  run: async (client, message, args) => {
 
- const url =  'https://no-api-key.com/api/v1/animals/panda'   
- const result = await axios.get(url)
+    const url = 'https://no-api-key.com/api/v1/animals/panda'
+    const result = await axios.get(url)
 
- const data =  result.data; 
+    const data = result.data;
 
- const embed = new Discord.MessageEmbed()
- .setAuthor(`${message.author.tag} Requested a panda `, message.author.displayAvatarURL({dynamic:true}))
- .setDescription(`**Panda Fact:**\n> ${data.fact}`)
- .setImage(data.image)
- .setColor('#A6FE00')
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(`${message.author.tag} Requested a panda `, message.author.displayAvatarURL({dynamic:true}))
+      .setDescription(`**Panda Fact:**\n> ${data.fact}`)
+      .setImage(data.image)
+      .setColor('#A6FE00')
 
-message.channel.send({embeds: [embed]})
-}
+    message.channel.send({embeds: [embed]})
+  }
 }
