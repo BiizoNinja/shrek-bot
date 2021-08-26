6bf1fd7121909ad894cbbf55659b8bea4a23ffde
Arche <66189242+arche-dev@users.noreply.github.com>
2021-08-26 23:07:31 +0000

    Ident dog.js

--- Commands/Image/dog.js
+++ Commands/Image/dog.js
@@ -2,22 +2,22 @@ const Discord = require('discord.js');
 const axios = require('axios')
 
 module.exports = {
-name: 'dog',
-description: 'Sends a dog image and a fact!',
-usage: 'dog',
-run: async (client, message, args) => {
+  name: 'dog',
+  description: 'Sends a dog image and a fact!',
+  usage: 'dog',
+  run: async (client, message, args) => {
 
- const url =  'https://no-api-key.com/api/v1/animals/dog'   
- const result = await axios.get(url)
+    const url = 'https://no-api-key.com/api/v1/animals/dog'
+    const result = await axios.get(url)
 
- const data =  result.data; 
+    const data = result.data;
 
- const embed = new Discord.MessageEmbed()
- .setAuthor(`${message.author.tag} Requested a dog `, message.author.displayAvatarURL({dynamic:true}))
- .setDescription(`**Dog Fact:**\n> ${data.fact}`)
- .setImage(data.image)
- .setColor('#A6FE00')
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(`${message.author.tag} Requested a dog `, message.author.displayAvatarURL({dynamic:true}))
+      .setDescription(`**Dog Fact:**\n> ${data.fact}`)
+      .setImage(data.image)
+      .setColor('#A6FE00')
 
-message.channel.send({embeds: [embed]})
-}
+    message.channel.send({embeds: [embed]})
+  }
 }
