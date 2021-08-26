0be5ee76295f533af7703c96f6bd84062f546825
Arche <66189242+arche-dev@users.noreply.github.com>
2021-08-26 23:06:12 +0000

    Ident cat.js

--- Commands/Image/cat.js
+++ Commands/Image/cat.js
@@ -2,22 +2,22 @@ const Discord = require('discord.js');
 const axios = require('axios')
 
 module.exports = {
-name: 'cat',
-description: 'Sends a cat image and a fact!',
-usage: 'cat',
-run: async (client, message, args) => {
+  name: 'cat',
+  description: 'Sends a cat image and a fact!',
+  usage: 'cat',
+  run: async (client, message, args) => {
+    
+   const url =  'https://no-api-key.com/api/v1/animals/cat'
+   const result = await axios.get(url)
 
- const url =  'https://no-api-key.com/api/v1/animals/cat'   
- const result = await axios.get(url)
+   const data =  result.data;
 
- const data =  result.data; 
+   const embed = new Discord.MessageEmbed()
+     .setAuthor(`${message.author.tag} Requested a cat `, message.author.displayAvatarURL({dynamic:true}))
+     .setDescription(`**Cat Fact:**\n> ${data.fact}`)
+     .setImage(data.image)
+     .setColor('#A6FE00')
 
- const embed = new Discord.MessageEmbed()
- .setAuthor(`${message.author.tag} Requested a cat `, message.author.displayAvatarURL({dynamic:true}))
- .setDescription(`**Cat Fact:**\n> ${data.fact}`)
- .setImage(data.image)
- .setColor('#A6FE00')
-
-message.channel.send({embeds: [embed]})
-}
+   message.channel.send({embeds: [embed]})
+  }
 }
