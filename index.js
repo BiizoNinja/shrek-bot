a86e56c479042a1f59cf945c9558f7f754d41945
Gonz <78351336+gonzyui@users.noreply.github.com>
2021-09-17 20:49:16 +0200

    simpler way to write intents

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 const { Client, Collection, Intents} = require("discord.js");
 
 const client = new Client({
-    intents: [
-        "GUILDS",
-        "GUILD_MEMBERS",
-        "GUILD_BANS",
-        "GUILD_INTEGRATIONS",
-        "GUILD_WEBHOOKS",
-        "GUILD_INVITES",
-        "GUILD_VOICE_STATES",
-        "GUILD_PRESENCES",
-        "GUILD_MESSAGES",
-        "GUILD_MESSAGE_REACTIONS",
-        "GUILD_MESSAGE_TYPING",
-        "DIRECT_MESSAGES",
-        "DIRECT_MESSAGE_REACTIONS",
-        "DIRECT_MESSAGE_TYPING",
-    ],
+    intents: Object.values(Intents.FLAGS)
 });
 
 module.exports = client;
